2a128677974698f72d22773f86ee137f559cc56d
ntaylor95 <ntaylorlssf@gmail.com>
2020-04-26 08:05:32 -0500

    simple api call

--- create-react-app/redux-practice/src/routes/Home.js
+++ create-react-app/redux-practice/src/routes/Home.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import axios from 'axios';
 
-const Home = () => {
-    return (
-        <div className='container'>
-            <h4 className='center'>Home</h4>
-            <p>lorem</p>
-        </div>
-    )
+class Home extends React.Component {
+    state = {
+        posts: []
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+            this.setState({
+                posts: res.data.slice(0,10)
+            })
+        })
+    }
+
+    render() {
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className='post card' key={post.id}>
+                        <div className='card-content'>
+                            <div className='card-title'>{post.title}</div>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className='center'>No Posts Yet</div>
+        );
+
+        return (
+            <div className='container'>
+                <h4 className='center'>Home</h4>
+                {postList}
+            </div>
+        )
+    }
 }
+
 export default Home;
